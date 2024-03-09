be134c164001fd39666ec8bf4439088e108e2ee9
volskyi-dmytro <volskyi.dmytro@gmail.com>
2024-03-09 22:22:04 +0200

    Initial commit

---
+++ src/test/java/com/uabookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uabookshop.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
